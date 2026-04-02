897b96e17a872639c4f05ef675e99564e185d55e
nilb7 <tieenn298@gmail.com>
2026-04-02 20:47:04 +0200

    sd

---
+++ lesson 2/main.py
@@ -0,0 +1,57 @@
+name = "Nil"
+age=15
+temperature=38.2
+surname="Bakija"
+
+full_name= name + surname
+
+
+
+
+
+
+
+users =["Andi", "Nili", "Olti", "Lisa"]
+
+personiFitues =users.pop()
+
+print(users)
+#users.insert(2, "ensari")
+#print(users)
+
+
+
+
+
+
+
+
+
+
+
+#print(users)
+#users.reverse()
+#print(users)
+
+
+
+#users.append("Edeni")
+#print(users)
+
+#users.clear()
+#print(users)
+
+#users.count("Andi")
+#print(users.count("Andi"))
+
+
+#print(full_name)
+#print(name)
+#print(age)
+#print(temperature)
+#print(type(name))
+
+#print(users[0])
+#print(users[1])
+#print(users[2])
+#print(users[3])
