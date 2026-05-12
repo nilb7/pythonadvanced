7754fb8eb2facfba19f54b0366f5f8587c4939c5
nilb7 <tieenn298@gmail.com>
2026-05-12 20:42:24 +0200

    tr

---
+++ lesson 10/main.py
@@ -0,0 +1,9 @@
+from student import Student
+
+
+student1 = Student("Nili", 15)
+
+print(student1.get_name())
+
+student1.set_name("Andiii")
+student1.set_age(17)
---
+++ lesson 10/student.py
@@ -0,0 +1,18 @@
+class Student:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+    def get_name(self):
+        return self.name
+
+    def age(self):
+        return self.age
+
+    def set_name(self,name):
+         self.name = name
+
+    def set_age(self,age):
+        self.age = age
