3fb355e77846287928b7b8e76684d18acbd5b5dd
nilb7 <tieenn298@gmail.com>
2026-06-02 20:46:44 +0200

    df

--- lesssonn16/kalkulatori.py
+++ lesssonn16/kalkulatori.py
@@ -1,12 +1,21 @@
 import streamlit as st
+from final_project3 import result_label
 
-def kalkulatori():
-    st.title("Simple Calculator")
 
-    num1 = st.number_input("Enter the first number",step=1)
+def kalkulo(num1,num2,operation):
+    if operation=="mbledhje":
+        result = num1+num2
+    elif operation=="zbrijte":
+        result=num1-num2
+    return  result
+st.title("Simple Calculator")
 
-    num2 = st.number_input("Enter the second number", step=1)
+num1 = st.number_input("Enter the first number",step=1)
+
+num2 = st.number_input("Enter the second number", step=1)
 
 
 operation = st.radio("selected operation",["mbledhje","zbrijte","shumezim","pjestim"])
 
+result = kalkulo(num1,num2,operation)
+st.write(result)
