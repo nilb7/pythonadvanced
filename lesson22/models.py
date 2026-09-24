acb55f760fe9f4d1f09f5ccbeffd8118d8e933c0
nilb7 <tieenn298@gmail.com>
2026-09-24 20:47:16 +0200

    sd
    sd

---
+++ lesson22/models.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, FieldValidationInfo, field_validator,constr, conint
+
+class User(BaseModel):
+    id:int
+    name:str
+    age:int
+
+    @field_validator("age")
+
+    def age_must_be_positive(cls,v,info:FieldValidationInfo):
+        if v <=0:
+            raise ValueError("Age must be positive")
+        return v
+
+try:
+    user = User(id=1,name="eglandin",age=-9)
+except ValueError as e :
+    print(e)
+
+class Adress(BaseModel):
+    street:str
+    city:str
